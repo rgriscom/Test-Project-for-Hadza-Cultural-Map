60244c0e385bd8c1e22e1e61074b05a39184d5a8
Richard T. Griscom <46093597+rgriscom@users.noreply.github.com>
2020-07-21 19:40:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ var config = {
         {
             id: 'tanzania',
             title: 'Introduction',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/2/26/Hadza_montage.png',
-            description: 'The Hadzabe community of the Yaida Valley requested <a href="http://www.ujamaa-crt.org/">UCRT</a> to assist them them to undertake a cultural mapping exercise. More than 200 Hadzabe, young and old, men and women, participated in this exercise during the months of October 2004 and October 2006. The goals of this exercise were among the following: a) to enable elders to impart to young people the deep connection between lang & culture through identifying and relating the stories of special sites; b) to show to outsiders & government leaders an ownership of land that only comes through a long historical relationship and a deep knowledge of the land; and c) in this vein to garner support for their rights to land and use of natural resources.',
+            image: '',
+            description: '<img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Hadza_montage.png" align="left" style=”margin: 10px 10px 10px 10px /> The Hadzabe community of the Yaida Valley requested <a href="http://www.ujamaa-crt.org/">UCRT</a> to assist them them to undertake a cultural mapping exercise. More than 200 Hadzabe, young and old, men and women, participated in this exercise during the months of October 2004 and October 2006. The goals of this exercise were among the following: a) to enable elders to impart to young people the deep connection between lang & culture through identifying and relating the stories of special sites; b) to show to outsiders & government leaders an ownership of land that only comes through a long historical relationship and a deep knowledge of the land; and c) in this vein to garner support for their rights to land and use of natural resources.',
             location: {
                 center: [-36.28278, -0.78869],
                 zoom: 1.88,
