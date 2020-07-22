f4efca71ca6d1340096702c089e623b05f8c34d2
Richard T. Griscom <46093597+rgriscom@users.noreply.github.com>
2020-07-22 10:26:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,7 @@ var config = {
             image: '',
             description: '<img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Hadza_montage.png" align="left" style=”margin: 10px 10px 10px 10px /> <p>The Hadzabe community of the Yaida Valley requested <a href="http://www.ujamaa-crt.org/">UCRT</a> to assist them them to undertake a cultural mapping exercise. More than 200 Hadzabe, young and old, men and women, participated in this exercise during the months of October 2004 and October 2006. The goals of this exercise were among the following: a) to enable elders to impart to young people the deep connection between lang & culture through identifying and relating the stories of special sites; b) to show to outsiders & government leaders an ownership of land that only comes through a long historical relationship and a deep knowledge of the land; and c) in this vein to garner support for their rights to land and use of natural resources.</p>',
             alignment: 'left',
+            opacity: 'normal',
             location: {
                 center: [-36.28278, -0.78869],
                 zoom: 1.88,
@@ -34,6 +35,7 @@ var config = {
             image: 'https://ssl.c.photoshelter.com/img-get/I0000b6HRoGzjkwY/s/850/Evolution-of-Diet-010.jpg',
             description: "<p>We Hadzabe have no record of famine in our oral history. The reason is that we depend on natural producs of the environment such as berries, tubers, baobab fruits, honey and many wild animals for food. By living in this way, the environment we depend on is not damaged and remains healthy.</p>",
             alignment: 'left',
+            opacity: 'normal',
             location: {
                 center: [31.47641, -6.90268],
                 zoom: 5.4,
@@ -57,6 +59,7 @@ var config = {
             image: 'https://www.dropbox.com/s/up0kju55n0y758n/bar_photos1.png?raw=1',
             description: "<p>But, because we move in search of seasonal food and other necessary natural products such as arrow poison, special stones for sharpening tools and for our smoking pipes, it has been easy for neighboring people to move into and take our land. We also are a peaceful people who do not like confrontation so that rather than fight people who invade we have always moved away. Also, because we do not plant crops or herd livestock, most people including government leaders, consider our lands to be empty and unused. They don't understand how we use and depend on the land and a healthy environment. The result is that we have lost 90% of our land within the last 50 years. And the land that has been lost to others is seriously degraded environmentally - it is very sick. </p><p><img src='https://www.dropbox.com/s/hs6eznpzxyy6kwa/bar_photos2.png?raw=1'></p>",
             alignment: 'center',
+            opacity: 'full',
             location: {
                 center: [31.47641, -6.90268],
                 zoom: 5.4,
@@ -80,6 +83,7 @@ var config = {
             image: '',
             description: "<p> Not only do we depend on the land and a healthy environment for our daily needs, loss of land also deprives us of our cultural identity and the means to move with dignity into the future world. Our right to modern education and health care must be based on a foundation of land and a healthy environment. We hope that this booklet helps others to understand and respect our need for and deep connection with the land and to support us in our quest for land & natural resource rights.</p>",
             alignment: 'center',
+            opacity: 'normal',
             location: {
                 center: [31.47641, -6.90268],
                 zoom: 5.4,
@@ -103,6 +107,7 @@ var config = {
             image: 'https://jimmynelsonfoundation.com/wp-content/uploads/2019/04/jimmy-nelson-foundation-hadzabe-tanzania-gallery-mike-holtby-6-groter.jpg',
             description: "Research based on blood samples and DNA comparisons of people all over the world indicate that we Hadzabe are on of the original peoples. We have probably lived in the Eyasi Basin and surrounding areas for at least 100,000 years, if not more. Our more recent origin story relates that the Isanzu Chief Kintentemi long ago invited the Hadzabe to a big feast. At that feast many Hadzabe were poisoned. The survivors moved to the East - ANAU; to the west - DILODABEE; and to the North - SANZATABEE. These three main groups of recent origin gave rise to all the geographic subgroups of Hadzabe found today - the Siponganebee, Dunduinebee, Tl'iikanabee, Mwasimwasikanebee & Mangolanebee.",
             alignment: 'left',
+            opacity: 'normal',
             location: {
                 center: [34.69620,  -3.67432],
                 zoom: 9.26,
@@ -122,6 +127,7 @@ var config = {
             image: '',
             description: 'After this peopling of the Hadzabe, there was a very difficult Isanzu famine. In order to survive, many Isanzu moved to live with the Hadzabe and benefit from the bush foods which are not affected by drought like the planted foods are. This movement led to intermarriage and also to the historically incorrect story that the Hadza originated from Ukerewe to the West, which is where the Isanzu but not the Hadza came from.',
             alignment: 'center',
+            opacity: 'normal',
             location: {
                 center: [34.69620,  -3.67432],
                 zoom: 9.26,
@@ -141,6 +147,7 @@ var config = {
             image: '',
             description: 'Land occupied by the Hadza 50 years ago (red)',
             alignment: 'left',
+            opacity: 'normal',
             location: {
                 center: [34.69620,  -3.67432],
                 zoom: 9.26,
@@ -166,6 +173,7 @@ var config = {
             image: '',
             description: 'Land the Hadza have rights to today (green)',
             alignment: 'left',
+            opacity: 'normal',
             location: {
                 center: [34.69620,  -3.67432],
                 zoom: 9.26,
@@ -199,6 +207,7 @@ var config = {
             image: '',
             description: 'Sites of cultural significance (blue)',
             alignment: 'left',
+            opacity: 'normal',
             location: {
                 center: [34.69620,  -3.67432],
                 zoom: 9.26,
