a610269036da799f6d27cac478a8fd294d9e9041
Richard T. Griscom <46093597+rgriscom@users.noreply.github.com>
2020-07-24 17:51:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -249,7 +249,7 @@ var config = {
             id: 'video',
             title: 'Video test',
             image: '',
-            description: '<iframe width="560" height="315" src="https://www.youtube.com/embed/MNi7ItWKjhw" frameborder="0" allow="accelerometer; autoplay="1"; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            description: '<iframe width="560" height="315" src="https://www.youtube.com/embed/MNi7ItWKjhw?autoplay=1" frameborder="0" allow="accelerometer; autoplay="1"; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
             alignment: 'left',
             opacity: 'normal',
             location: {
