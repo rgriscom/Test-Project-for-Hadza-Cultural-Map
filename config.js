95245677a835f90410d67812d2fdc3e8992ea105
Richard T. Griscom <46093597+rgriscom@users.noreply.github.com>
2020-07-28 20:17:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -256,7 +256,7 @@ var config = {
         },
         {
             id: 'video',
-            title: 'Video test',
+            title: '',
             image: '',
             description: '<video autoplay loop id="myVideo"><source src="https://uce9685cf17b4a55054851313020.dl.dropboxusercontent.com/cd/0/inline/A8Z7s8MbcDfeNB_wbdqCHhrY-qdGggQU7OqGDzq6nKRlVinY7pXxfwUtz1SUuBG2MUYMYuhhNis3a4uyjin32hBw8-RIDSfBvdD7AD0qN6Xvj11CvWaaiysQCEGWHbJBYsk/file#" type="video/mp4"></video><div id="myContent"><h1>Foraging Compilation</h1><p>The Hadza forage for honey and roots.</p></div>',
             alignment: 'center',
