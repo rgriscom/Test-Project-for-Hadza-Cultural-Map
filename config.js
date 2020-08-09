05d0b01015fdce918629289d8992c2f32d3c1897
Richard T. Griscom <46093597+rgriscom@users.noreply.github.com>
2020-08-09 15:31:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var config = {
     theme: 'light',
     title: '<center>Original People</center>',
     subtitle: '<center>Land, Culture, History & Destiny</center>',
-    byline: '<center><i>An interactive story</i></center>',
+    byline: '<center><i>An interactive story</i></center><p><center><button id="switch-lang">Switch Language / Badilisha Lugha</button></center>',
     footer: '<a href="https://rgriscom.github.io/Test-Project-for-Hadza-Cultural-Map-2/">Click here to view all of areas of cultural significance to the Hadza </a>',
     chapters: [
         {
